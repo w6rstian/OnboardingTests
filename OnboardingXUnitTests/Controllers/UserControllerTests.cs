4f0cf060abff3c3f69c8a8cbb0a6afacd75e1cc8
wojtekjurkowicz <wojtek.jurkowicz@gmail.com>
2026-03-12 22:09:58 +0100

    added 30 tests: 15 in UserControllerTests and 15 in UserTests

--- OnboardingXUnitTests/Controllers/UserControllerTests.cs
+++ OnboardingXUnitTests/Controllers/UserControllerTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Security.Claims;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using FakeItEasy;
 using FluentAssertions;
@@ -12,6 +13,8 @@ using Onboarding.Models;
 using Onboarding.Interfaces;
 using Xunit;
 using Task = System.Threading.Tasks.Task;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc.Routing;
 
 namespace OnboardingXUnitTests.Controllers
 {
@@ -30,6 +33,13 @@ namespace OnboardingXUnitTests.Controllers
             _fakeEmailSender = A.Fake<IEmailSender>();
 
             _controller = new UserController(_fakeUserManager, _fakeEmailSender, _fakeUserStore);
+
+            var fakeUrlHelper = A.Fake<IUrlHelper>();
+            _controller.Url = fakeUrlHelper;
+
+            var tempDataProvider = A.Fake<ITempDataProvider>();
+            var tempDataDictionaryFactory = new TempDataDictionaryFactory(tempDataProvider);
+            _controller.TempData = tempDataDictionaryFactory.GetTempData(new DefaultHttpContext());
 
             var user = new ClaimsPrincipal(new ClaimsIdentity(new[]
             {
@@ -61,6 +71,233 @@ namespace OnboardingXUnitTests.Controllers
 
             // Assert
             result.Should().BeOfType<ViewResult>();
+        }
+
+        // New tests - MyAccount
+        [Fact]
+        public async Task MyAccount_Get_ReturnsViewWithUser()
+        {
+            // Arrange - checks if returns user
+            var testUser = new User { Name = "Jan" };
+            A.CallTo(() => _fakeUserManager.GetUserAsync(A<ClaimsPrincipal>._)).Returns(Task.FromResult(testUser));
+
+            // Act
+            var result = await _controller.MyAccount();
+
+            // Assert 
+            var viewResult = result.Should().BeOfType<ViewResult>().Subject;
+            viewResult.Model.Should().Be(testUser);
+        }
+
+        [Fact]
+        public async Task MyAccount_Post_ReturnsNotFound_WhenUserNull()
+        {
+            // Arrange - checks whether user is null
+            A.CallTo(() => _fakeUserManager.GetUserAsync(A<ClaimsPrincipal>._)).Returns(Task.FromResult<User>(null));
+
+            // Act
+            var result = await _controller.MyAccount("Jan", "K", "j@j.pl", "123", "IT", "Dev");
+
+            // Assert
+            result.Should().BeOfType<NotFoundResult>();
+        }
+
+        [Fact]
+        public async Task MyAccount_Post_ReturnsRedirect_OnSuccess()
+        {
+            // Arrange
+            var testUser = new User { Id = 1, Email = "test@test.com" };
+            A.CallTo(() => _fakeUserManager.GetUserAsync(A<ClaimsPrincipal>._)).Returns(Task.FromResult(testUser));
+            A.CallTo(() => _fakeUserManager.UpdateAsync(A<User>._)).Returns(Task.FromResult(IdentityResult.Success));
+            A.CallTo(() => _fakeUserManager.GetUserIdAsync(A<User>._)).Returns(Task.FromResult(testUser.Id.ToString()));
+            A.CallTo(() => _fakeUserManager.GenerateEmailConfirmationTokenAsync(A<User>._)).Returns(Task.FromResult("token123"));
+
+            // Act
+            var result = await _controller.MyAccount("Jan", "K", "j@j.pl", "123", "IT", "Dev");
+
+            // Assert
+            var redirect = result.Should().BeOfType<RedirectToActionResult>().Subject;
+            redirect.ActionName.Should().Be("MyAccount");
+
+            // Checks if mail was sent
+            A.CallTo(() => _fakeEmailSender.SendEmailAsync(A<string>._, A<string>._, A<string>._)).MustHaveHappened();
+        }
+
+        [Fact]
+        public async Task MyAccount_Post_ReturnsView_WhenUpdateFails()
+        {
+            // Arrange
+            var testUser = new User { Id = 1 };
+            var identityError = IdentityResult.Failed(new IdentityError { Description = "B³¹d zapisu" });
+            A.CallTo(() => _fakeUserManager.GetUserAsync(A<ClaimsPrincipal>._)).Returns(Task.FromResult(testUser));
+            A.CallTo(() => _fakeUserManager.UpdateAsync(A<User>._)).Returns(identityError);
+
+            // Act
+            var result = await _controller.MyAccount("Jan", "K", "j@j.pl", "123", "IT", "Dev");
+
+            // Assert
+            result.Should().BeOfType<ViewResult>();
+            _controller.ModelState.ErrorCount.Should().BeGreaterThan(0);
+        }
+
+        [Theory] // 3 tests
+        [InlineData("", "Kowalski")]
+        [InlineData("Jan", "")]
+        [InlineData(null, null)]
+        public async Task MyAccount_Post_HandlesMissingNames(string n, string ln)
+        {
+            // Arrange
+            var testUser = new User { Id = 1 };
+            A.CallTo(() => _fakeUserManager.GetUserAsync(A<ClaimsPrincipal>._)).Returns(testUser);
+            A.CallTo(() => _fakeUserManager.UpdateAsync(A<User>._)).Returns(IdentityResult.Success);
+            A.CallTo(() => _fakeUserManager.GetUserIdAsync(A<User>._)).Returns("1");
+
+            // Act
+            var result = await _controller.MyAccount(n, ln, "test@o2.pl", "111", "IT", "Dev");
+
+            // Assert
+            result.Should().BeOfType<RedirectToActionResult>();
+        }
+
+        [Fact]
+        public async Task MyAccount_Post_ReturnsRedirect_EvenIfUserIdIsNull()
+        {
+            // Arrange - checks if GetUserIdAsync throws error/null
+            var testUser = new User { Id = 1 };
+            A.CallTo(() => _fakeUserManager.GetUserAsync(A<ClaimsPrincipal>._)).Returns(testUser);
+            A.CallTo(() => _fakeUserManager.UpdateAsync(A<User>._)).Returns(IdentityResult.Success);
+            A.CallTo(() => _fakeUserManager.GetUserIdAsync(A<User>._)).Returns(Task.FromResult<string>(null));
+
+            // Act
+            var result = await _controller.MyAccount("J", "K", "j@j.pl", "1", "D", "P");
+
+            // Assert
+            result.Should().BeOfType<RedirectToActionResult>();
+        }
+
+        [Fact]
+        public async Task MyAccount_Post_AddsMultipleErrorsToModelState()
+        {
+            // Arrange - checks if UpdateAsync throws many errors
+            var testUser = new User { Id = 1 };
+            var identityError = IdentityResult.Failed(
+                new IdentityError { Description = "Error 1" },
+                new IdentityError { Description = "Error 2" }
+            );
+            A.CallTo(() => _fakeUserManager.GetUserAsync(A<ClaimsPrincipal>._)).Returns(testUser);
+            A.CallTo(() => _fakeUserManager.UpdateAsync(A<User>._)).Returns(identityError);
+
+            // Act
+            await _controller.MyAccount("a", "b", "c", "d", "e", "f");
+
+            // Assert
+            _controller.ModelState.ErrorCount.Should().Be(2);
+        }
+
+        // Tests of UserSettings
+        [Fact]
+        public async Task UserSettings_Post_UpdatesData_AndRedirects()
+        {
+            // Arrange
+            var testUser = new User { Name = "Stare" };
+            var model = new User { Name = "Nowe", Surname = "Nazwisko", PhoneNumber = "999" };
+
+            A.CallTo(() => _fakeUserManager.GetUserAsync(A<ClaimsPrincipal>._)).Returns(Task.FromResult(testUser));
+            A.CallTo(() => _fakeUserManager.UpdateAsync(A<User>._)).Returns(Task.FromResult(IdentityResult.Success));
+
+            // Act
+            var result = await _controller.UserSettings(model);
+
+            // Assert
+            testUser.Name.Should().Be("Nowe");
+            result.Should().BeOfType<RedirectToActionResult>();
+        }
+
+        [Fact]
+        public async Task UserSettings_Post_ReturnsView_WhenUpdateFails()
+        {
+            // Arrange
+            var testUser = new User { Name = "Stare" };
+            var identityError = IdentityResult.Failed(new IdentityError { Description = "B³¹d bazy" });
+
+            A.CallTo(() => _fakeUserManager.GetUserAsync(A<ClaimsPrincipal>._)).Returns(Task.FromResult(testUser));
+            A.CallTo(() => _fakeUserManager.UpdateAsync(A<User>._)).Returns(Task.FromResult(identityError));
+
+            // Act
+            var result = await _controller.UserSettings(new User());
+
+            // Assert
+            result.Should().BeOfType<ViewResult>();
+            _controller.ModelState.IsValid.Should().BeFalse(); // Checks if error is in ModelState
+        }
+
+        [Fact]
+        public async Task UserSettings_Get_ReturnsViewWithUser()
+        {
+            // Arrange
+            var testUser = new User { Id = 1, Name = "Marek" };
+            A.CallTo(() => _fakeUserManager.GetUserAsync(A<ClaimsPrincipal>._)).Returns(testUser);
+
+            // Act
+            var result = await _controller.UserSettings();
+
+            // Assert
+            var viewResult = result.Should().BeOfType<ViewResult>().Subject;
+            viewResult.Model.Should().Be(testUser);
+        }
+
+        [Fact]
+        public async Task UserSettings_Post_ReturnsNotFound_WhenUserIsNull()
+        {
+            // Arrange
+            A.CallTo(() => _fakeUserManager.GetUserAsync(A<ClaimsPrincipal>._)).Returns(Task.FromResult<User>(null));
+
+            // Act
+            var result = await _controller.UserSettings(new User());
+
+            // Assert
+            result.Should().BeOfType<NotFoundResult>();
+        }
+
+        [Fact]
+        public async Task UserSettings_Post_SetsTempData_OnSuccess()
+        {
+            // Arrange
+            var testUser = new User { Id = 1 };
+            A.CallTo(() => _fakeUserManager.GetUserAsync(A<ClaimsPrincipal>._)).Returns(testUser);
+            A.CallTo(() => _fakeUserManager.UpdateAsync(A<User>._)).Returns(IdentityResult.Success);
+
+            // Act
+            await _controller.UserSettings(new User { Name = "Nowe" });
+
+            // Assert
+            _controller.TempData["SuccessMessage"].Should().Be("Dane u¿ytkownika zosta³y zaktualizowane.");
+        }
+
+        [Fact]
+        public async Task UserSettings_Post_UpdatesAllFieldsCorrectly()
+        {
+            // Arrange
+            var testUser = new User { Id = 1 };
+            var model = new User
+            {
+                Name = "A",
+                Surname = "B",
+                PhoneNumber = "123",
+                Department = "IT",
+                Position = "Dev"
+            };
+            A.CallTo(() => _fakeUserManager.GetUserAsync(A<ClaimsPrincipal>._)).Returns(testUser);
+            A.CallTo(() => _fakeUserManager.UpdateAsync(A<User>._)).Returns(IdentityResult.Success);
+
+            // Act
+            await _controller.UserSettings(model);
+
+            // Assert
+            testUser.Surname.Should().Be("B");
+            testUser.PhoneNumber.Should().Be("123");
+            testUser.Department.Should().Be("IT");
+            testUser.Position.Should().Be("Dev");
         }
     }
 }
---
+++ OnboardingXUnitTests/Models/UserTests.cs
@@ -0,0 +1,187 @@
+using Xunit;
+using FluentAssertions;
+using Onboarding.Models;
+using System.Collections.Generic;
+
+namespace OnboardingXUnitTests.Models
+{
+    public class UserTests
+    {
+        // 5 tests
+        [Theory]
+        [InlineData("Jan", "Kowalski", "IT", "Developer")]
+        [InlineData("Anna", "Nowak", "HR", "Recruiter")]
+        [InlineData(null, null, null, null)]
+        [InlineData("", "", "", "")]
+        [InlineData("BardzoDlugieImieDlaTestu", "Nazwisko", "Department", "Pos")]
+        public void User_BasicProperties_ShouldStoreValues(string name, string surname, string dept, string pos)
+        {
+            // Arrange & Act
+            var user = new User
+            {
+                Name = name,
+                Surname = surname,
+                Department = dept,
+                Position = pos
+            };
+
+            // Assert
+            user.Name.Should().Be(name);
+            user.Surname.Should().Be(surname);
+            user.Department.Should().Be(dept);
+            user.Position.Should().Be(pos);
+        }
+
+        // Test Login (Property Login)
+        [Fact]
+        public void User_Login_Property_ShouldWork()
+        {
+            // Arrange
+            var user = new User();
+
+            // Act
+            user.Login = "jkowalski";
+
+            // Assert
+            user.Login.Should().Be("jkowalski");
+        }
+
+        [Fact]
+        public void User_BuddyId_ShouldBeNullable()
+        {
+            // Arrange & Act
+            var user = new User { BuddyId = null };
+
+            // Assert
+            user.BuddyId.Should().BeNull();
+        }
+
+        [Fact]
+        public void User_Buddy_ShouldAllowSettingBuddyObject()
+        {
+            // Arrange
+            var buddyUser = new User { Id = 10, Name = "Opiekun" };
+
+            // Act
+            var user = new User { Buddy = buddyUser, BuddyId = 10 };
+
+            // Assert
+            user.Buddy.Name.Should().Be("Opiekun");
+        }
+
+        [Fact]
+        public void User_BuddyRelation_CanBeChanged()
+        {
+            // Arrange
+            var user = new User { BuddyId = 1 };
+
+            // Act
+            user.BuddyId = 2;
+
+            // Assert
+            user.BuddyId.Should().Be(2);
+        }
+
+        [Fact]
+        public void User_InheritedIdentityProperties_ShouldWork()
+        {
+            // Testing inheritence after IdentityUser<int> 
+
+            // Arrange & Act
+            var user = new User { Email = "test@test.pl", PhoneNumber = "123456789" };
+
+            // Assert
+            user.Email.Should().Be("test@test.pl");
+            user.PhoneNumber.Should().Be("123456789");
+        }
+
+        // testing collections (6 tests)
+        [Fact]
+        public void User_Collections_ShouldNotBeNull_WhenAssigned()
+        {
+            // Arrange
+            var user = new User
+            {
+                UserCourses = new List<UserCourse>(),
+                SentMessages = new List<Message>(),
+                ReceivedMessages = new List<Message>(),
+                GivenRewards = new List<Reward>(),
+                ReceivedRewards = new List<Reward>(),
+                Notifications = new List<Notification>()
+            };
+
+            // Act & Assert
+            user.UserCourses.Should().NotBeNull();
+            user.SentMessages.Should().NotBeNull();
+            user.ReceivedMessages.Should().NotBeNull();
+            user.Notifications.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void User_UserCourses_ShouldStoreItems()
+        {
+            // Arrange
+            var user = new User { UserCourses = new List<UserCourse>() };
+            var course = new UserCourse { CourseId = 1 };
+
+            // Act
+            user.UserCourses.Add(course);
+
+            // Assert
+            user.UserCourses.Should().Contain(course);
+        }
+
+        [Fact]
+        public void User_Announcements_ShouldStoreItems()
+        {
+            // Arrange
+            var user = new User { Announcements = new List<Announcement>() };
+            var announcement = new Announcement { Id = 1 };
+
+            // Act
+            user.Announcements.Add(announcement);
+
+            // Assert
+            user.Announcements.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public void User_GivenRewards_ShouldStoreItems()
+        {
+            // Arrange
+            var user = new User { GivenRewards = new List<Reward>() };
+            var reward = new Reward
+            {
+                Id = 1,
+                Feedback = "Œwietna robota!",
+                Rating = 5
+            };
+
+            // Act
+            user.GivenRewards.Add(reward);
+
+            // Assert
+            user.GivenRewards.Should().Contain(reward).And.HaveCount(1);
+        }
+
+        [Fact]
+        public void User_ReceivedRewards_ShouldStoreItems()
+        {
+            // Arrange
+            var user = new User { ReceivedRewards = new List<Reward>() };
+            var reward = new Reward
+            {
+                Id = 5,
+                Feedback = "Pomocny kolega",
+                Rating = 4
+            };
+
+            // Act
+            user.ReceivedRewards.Add(reward);
+
+            // Assert
+            user.ReceivedRewards.Should().HaveCount(1);
+            user.ReceivedRewards.First().Feedback.Should().Be("Pomocny kolega");
+        }
+    }
+}
