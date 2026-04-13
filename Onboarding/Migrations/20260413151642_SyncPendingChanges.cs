6839118e4e1cbab1b1b2c26df6bea000d61619eb
wojtekjurkowicz <wojtek.jurkowicz@gmail.com>
2026-04-13 17:19:17 +0200

    fix workflow v5 + added migrations

---
+++ Onboarding/Migrations/20260413151642_SyncPendingChanges.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Onboarding.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncPendingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
